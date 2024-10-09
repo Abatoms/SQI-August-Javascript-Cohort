f4769e43d3748ed72a89c5624ec2a422b1852990
Bolarinwa Ahmed <bolarinwaahmed22@gmail.com>
2024-10-09 12:02:29 +0100

    More class on classess

--- OOP/classes.js
+++ OOP/classes.js
@@ -8,38 +8,57 @@ console.log("Welcome to OOP in JS");
 
 // Class in JS
 class MyCar {
-  // Properties (Attributes)
-  carName = "Toyota";
-  carModel = "Camry";
-  carYear = 2022;
-
-  // Methods (Behaviours)
-  start() {
-    console.log("Car Started");
+  // Always run when an object is created from the class
+  constructor(name, brand) {
+    this.name = name;
+    this.brand = brand;
+    this.numOfLegs = 4;
+    this.color = "black";
   }
 
-  drive() {
-    console.log("Car Driving");
+  // Setters and Getters
+  // setters
+  setColor(colorInput) {
+    this.color = colorInput;
   }
 
-  brake() {
-    console.log("Car Braking");
-  }
-
-  stop() {
-    console.log("Car Stopped");
+  // getter
+  getColor() {
+    return this.color;
   }
 }
 
-// objects of the class
-const bolaCar = new MyCar();
-console.log(bolaCar);
-console.log(bolaCar.carModel);
-bolaCar.start();
+const bolaCar = new MyCar("Camry", "Toyots");
+const daraCar = new MyCar("Civic", "Honda");
+console.log(bolaCar.brand);
+console.log(daraCar.brand);
+console.log(bolaCar.numOfLegs + " - " + daraCar.numOfLegs);
+bolaCar.setColor("Gold");
+console.log("Bola car colour", bolaCar.getColor());
+console.log("Dara car colour", daraCar.getColor());
+daraCar.setColor("Green");
+console.log("Dara car colour", daraCar.getColor());
 
-const salamCar = new MyCar();
-salamCar.carName = "Honda";
-console.log(salamCar);
-salamCar.carModel = "Civic";
-console.log(salamCar.carModel);
-console.log(salamCar);
+// Create a class called Person. The class should have a function that is automatically called when you create a new instance of the class. The function should set the class properties for the name, age, and gender of the instance. Create a setter to modify the value of the name, and a getter to get the value of the gender
+
+class Person {
+  constructor(name, age, gender) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+
+  getGender() {
+    return this.gender;
+  }
+}
+
+const bola = new Person("Bolarinwa", 16, "Proudly Male");
+console.log(bola);
+bola.setName("Abolarinwa");
+console.log(bola.getGender());
+console.log("Accessing Gender", bola.gender);
