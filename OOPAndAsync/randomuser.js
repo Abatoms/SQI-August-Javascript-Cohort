6e25cd21c0897cb2c1ab799c5047815cdf4d97b4
Bolarinwa Ahmed <bolarinwaahmed22@gmail.com>
2024-10-17 11:07:46 +0100

    Fetching multiple random users

--- OOPAndAsync/randomuser.js
+++ OOPAndAsync/randomuser.js
@@ -36,3 +36,34 @@ async function fetchUserData() {
 fetchUserData();
 
 document.getElementById("generate").addEventListener("click", fetchUserData);
+
+// Getting multiple users
+const fetchMultipleUsers = async () => {
+  const response = await fetch("https://randomuser.me/api?results=20");
+  console.log(response);
+  if (response.ok) {
+    const data = await response.json();
+    // console.log("Data is ", data);
+    const users = data.results;
+    // console.log("The users are", users);
+    usersDiv = document.getElementById("users");
+    users.forEach((user) => {
+      usersDiv.innerHTML += `
+      <div class="user ${user.gender === "male" ? "male" : "female"}">
+            <img src=${user.picture.large} alt="User Image" />
+      <div>
+        <h1>${user.name.title} ${user.name.first} ${user.name.last}</h1>
+        <p><strong>Email:</strong> ${user.email}</p>
+        <p><strong>Gender:</strong> ${user.gender}</p>
+        <p><strong>Age:</strong> ${user.dob.age}</p>
+        <p><strong>Phone:</strong> ${user.phone}/p>
+      </div>
+      </div>
+      `;
+    });
+  } else {
+    console.log("An error occurred while fetching users data", err);
+  }
+};
+
+fetchMultipleUsers();
