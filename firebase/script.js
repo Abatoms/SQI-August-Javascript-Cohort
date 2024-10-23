7b437b47edd726c9c201a23e0d454c91e32e44fb
Bolarinwa Ahmed <bolarinwaahmed22@gmail.com>
2024-10-23 11:54:54 +0100

    Implemented another page for add and also delete functionality

--- firebase/script.js
+++ firebase/script.js
@@ -15,6 +15,7 @@ const db = firebase.firestore(app);
 
 const studentColRef = db.collection("students");
 const staffColRef = db.collection("staffs");
+let loading;
 
 // Get all students from the database
 const studentDiv = document.getElementById("students");
@@ -24,13 +25,17 @@ const lastname = document.getElementById("lastname");
 const age = document.getElementById("age");
 const level = document.getElementById("level");
 const admission_year = document.getElementById("admission_year");
+
 const getAllStudents = async () => {
-  const snapshot = await studentColRef.get();
+  let studentArray = [];
+  console.log(snapshot);
   try {
+    loading = true;
     //   console.log(snapshot);
     snapshot.forEach((doc) => {
-      console.log(doc.id);
-      console.log(doc.data());
+      studentArray.push({ ...doc.data(), id: doc.id });
+      // console.log(doc.id);
+      // console.log(doc.data());
       studentDiv.innerHTML += `
         <div class="student">
             <h1>${doc.data().firstname} ${doc.data().lastname}</h1>
@@ -39,18 +44,23 @@ const getAllStudents = async () => {
              <p class="admission-year"><strong>Admission Year:</strong> ${
                doc.data().admission_year
              }</p>
-         </div>
+            <button class="deleteBtn_${doc.id}">Delete</button>
+        </div>
     `;
     });
+    return studentArray;
   } catch (error) {
     console.log(
       "An error occured when fetching students with message: ",
       error.message
     );
+  } finally {
+    loading = false;
   }
 };
 
-getAllStudents();
+const students = await getAllStudents();
+// console.log(students);
 
 const addNewStudent = async () => {
   try {
@@ -64,6 +74,7 @@ const addNewStudent = async () => {
     console.log(data);
     const result = await studentColRef.add(data);
     console.log("Student added successfully? ", result);
+    window.location.href = "index.html";
   } catch (error) {
     console.log(
       "An error occured when adding student with message: ",
@@ -72,19 +83,37 @@ const addNewStudent = async () => {
   }
 };
 
-studentForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  addNewStudent();
+// console.log(students);
+
+// Delete Student
+const deleteStudent = async (id) => {
+  const studentDocRef = studentColRef.doc(id);
+  console.log("Deleting student with id: ", id);
+  console.log(studentDocRef);
+  try {
+    const result = await studentDocRef.delete();
+    console.log("Student deleted successfully? ", result);
+    window.location.href = "index.html";
+  } catch (error) {
+    console.log(
+      "An error occured when deleting student with message: ",
+      error.message
+    );
+  }
+};
+
+students.forEach((student) => {
+  // console.log(student);
+  const deleteBtn = document.querySelector(`.deleteBtn_${student.id}`);
+  deleteBtn.addEventListener("click", () => {
+    console.log("heyyy");
+    deleteStudent(student.id);
+  });
 });
 
-// const getAllStaffs = async () => {
-//   const snapshot = staffColRef.get();
-// };
-
-// addNewStudent({
-//   firstname: "Jinadu",
-//   lastname: "Elesho",
-//   age: 67,
-//   level: "300",
-//   admission_year: new Date(),
-// });
+if (studentForm) {
+  studentForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addNewStudent();
+  });
+}
